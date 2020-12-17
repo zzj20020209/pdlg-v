8be02563dad0e2d584f25564f0cde4f429bd6bbf
qq283777988 <283777988@qq.com>
2020-12-17 16:17:42 +0800

    配置了图片上传

--- src/main.js
+++ src/main.js
@@ -8,9 +8,10 @@ import App from "./App";
 
 Vue.use(ElementUI)
 
-Axios.defaults.baseURL = "http://localhost:8080/pdlg"
+Axios.defaults.baseURL = "http://localhost:8080/"
 // 将baseURL绑定到全局
 Vue.prototype.$axios = Axios
+Vue.prototype.$host = "http://localhost:8080/";
 new Vue({
   router,
   el: '#app',
