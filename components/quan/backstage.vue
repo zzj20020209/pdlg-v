f34399e04c02e60f150d06be0cc36c996aa5c61d
xmnh <fss1550837448@vip.qq.com>
2020-12-24 15:51:02 +0800

    后台当前时间

--- components/quan/backstage.vue
+++ components/quan/backstage.vue
@@ -7,7 +7,7 @@
           <br>
           欢迎：{{username}}
           <br>
-          当前时间:
+          当前时间:{{dateFormat(date)}}
           <br>
           <br>
           <a href="#" @click="zx">注销</a>
@@ -22,14 +22,14 @@
           background-color="whitesmoke"
           active-text-color="#ffd04b">
           <template v-for="menu in this.menuData">
-            <el-submenu :key="menu.id" :index="menu.id">
+            <el-submenu :key="menu.id" :index="menu.id+''">
               <template slot="title">
                 <!-- <i :class="menu.icon"></i>-->
                 <span slot="title">{{ menu.lable }}</span>
               </template>
               <el-menu-item-group>
                 <div v-for="cmenu in menu.childMenu">
-                  <el-menu-item @click="addTab(cmenu.lable,cmenu.linkUrl)" :key="cmenu.id" :index="cmenu.id">
+                  <el-menu-item @click="addTab(cmenu.lable,cmenu.linkUrl)" :key="cmenu.id" :index="cmenu.id+''">
                     <!--<i :class="cmenu.icon"></i>-->
                     <span slot="title">{{ cmenu.lable }}</span>
                   </el-menu-item>
@@ -62,10 +62,12 @@ import Sh from "../Shanghu/Sh";
 import ShSp from "../Shanghu/ShSp";
 import role from "./role";
 
+import UserList from "../User/UserList";
 export default {
   name: "backstage",
   data() {
     return {
+      date: new Date(),
       menuChild: [],
       menuData: [],
       editableTabsValue: '1',
@@ -80,9 +82,27 @@ export default {
     Sh,
     Goods,
     ShSp,
-    role
+    role,
+    UserList
   },
   methods: {
+    date(){
+      return Date.now();
+    },
+    dateFormat(time) {
+      let date=new Date(time);
+      let year=date.getFullYear();
+      /* 在日期格式中，月份是从0开始的，因此要加0
+      * 使用三元表达式在小于10的前面加0，以达到格式统一  如 09:11:05
+      * */
+      let month= date.getMonth()+1<10 ? "0"+(date.getMonth()+1) : date.getMonth()+1;
+      let day=date.getDate()<10 ? "0"+date.getDate() : date.getDate();
+      let hours=date.getHours()<10 ? "0"+date.getHours() : date.getHours();
+      let minutes=date.getMinutes()<10 ? "0"+date.getMinutes() : date.getMinutes();
+      let seconds=date.getSeconds()<10 ? "0"+date.getSeconds() : date.getSeconds();
+      // 拼接
+      return year+"-"+month+"-"+day+" "+hours+":"+minutes+":"+seconds;
+    },
     getDate() {
       let _this = this;
       this.$axios.post("/queryMenuAll.action").then(function (result) {
@@ -131,7 +151,19 @@ export default {
   },
   created() {
     this.getDate();
-  }
+  },
+  mounted() {
+    //显示当前日期时间
+    let _this = this// 声明一个变量指向Vue实例this，保证作用域一致
+    this.timer = setInterval(() => {
+      _this.date = new Date(); // 修改数据date
+    }, 1000)
+  },
+  beforeDestroy() {
+    if (this.timer) {
+      clearInterval(this.timer); // 在Vue实例销毁前，清除我们的定时器
+    }
+  },
 }
 </script>
 
